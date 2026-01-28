02f97447e0bdbfddd3f2c7db7d37267969ae7012
DaleMcBride211 <dalemcbride129@gmail.com>
2026-01-28 09:34:06 -0700

    Update server.js
    added cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const mongodb = require('./db/connect');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
