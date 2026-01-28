49fd18a5f2df528c51d6adde72363055a4698f49
DaleMcBride211 <dalemcbride129@gmail.com>
2026-01-28 09:42:24 -0700

    Update swagger.js
    trying to fix path issues

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'contacts information',
   },
   host: 'cse341-webservices-fk8a.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
